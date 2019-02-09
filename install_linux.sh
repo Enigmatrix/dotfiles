47e99ae9a5352f6031a9019a4b0f098454332866
Enigmatrix <enigmatrix2000@gmail.com>
2019-02-09 14:06:57 +0800

    install script

--- install_linux.sh
+++ install_linux.sh
@@ -1,5 +1,5 @@
 curl https://foosoft.net/projects/homemaker/dl/homemaker_linux_amd64.tar.gz /tmp/homemaker.tar.gz
-tar xzf /tmp/homemaker.tar.gz -c /tmp
+tar -xzf /tmp/homemaker.tar.gz -C /tmp
 git clone https://github.com/Enigmatrix/dotfiles.git ~/.dotfiles
 /tmp/homemaker -task $TARGET ~/.dotfiles/homemaker.toml ~/.dotfiles
 
