343055013157e44b22645abadf0773d36b9125e5
Enigmatrix <enigmatrix2000@gmail.com>
2019-02-09 14:12:31 +0800

    wtf

--- install_linux.sh
+++ install_linux.sh
@@ -1,5 +1,5 @@
-curl https://foosoft.net/projects/homemaker/dl/homemaker_linux_amd64.tar.gz /tmp/homemaker.tar.gz
-tar -xzf /tmp/homemaker.tar.gz -C /tmp
+curl https://foosoft.net/projects/homemaker/dl/homemaker_linux_amd64.tar.gz --output /tmp/homemaker.tar.gz
+tar -xzf /tmp/homemaker.tar.gz -C /tmp/hom
 git clone https://github.com/Enigmatrix/dotfiles.git ~/.dotfiles
 /tmp/homemaker -task $TARGET ~/.dotfiles/homemaker.toml ~/.dotfiles
 
