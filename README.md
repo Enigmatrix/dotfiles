9fa26d4d7a6aab4bcd6112af7e5c1baf97705f5e
Enigmatrix <enigmatrix2000@gmail.com>
2019-10-28 16:58:40 +0800

    add install files

---
+++ README.md
@@ -0,0 +1,8 @@
+# Enigmatrix's dotfiles
+
+## Installation
+
+### Linux-PC
+### Linux-NS
+### Linux-VM
+### Windows
---
+++ install_linux_ns.sh
@@ -0,0 +1,6 @@
+curl https://foosoft.net/projects/homemaker/dl/homemaker_linux_amd64.tar.gz --output /tmp/homemaker.tar.gz
+tar -xzf /tmp/homemaker.tar.gz -C /tmp
+sudo cp /tmp/homemaker_linux_amd64/homemaker /bin/homemaker
+git clone https://github.com/Enigmatrix/dotfiles.git ~/.dotfiles
+$(homemaker --verbose --task=linux_ns ~/.dotfiles/homemaker.toml ~/.dotfiles)
+
