2ba76645b5d09c2e167fbe8614c0b4d01428c79e
Enigmatrix <enigmatrix2000@gmail.com>
2020-03-15 14:03:16 +0800

    yee

--- linux_ns.sh
+++ linux_ns.sh
@@ -1,7 +1,7 @@
 curl https://foosoft.net/projects/homemaker/dl/homemaker_linux_amd64.tar.gz --output /tmp/homemaker.tar.gz
 tar -xzf /tmp/homemaker.tar.gz -C /tmp
-sudo cp /tmp/homemaker_linux_amd64/homemaker /bin/homemaker
+sudo cp /tmp/homemaker_linux_amd64/homemaker /usr/local/bin/homemaker
 git clone https://github.com/Enigmatrix/dotfiles.git ~/.dotfiles
-cd ~/.dotfiles && git checkout stow && cd ..
+cd ~/.dotfiles && cd ..
 $(homemaker --verbose --task=linux_ns ~/.dotfiles/homemaker.toml ~/.dotfiles)
 
